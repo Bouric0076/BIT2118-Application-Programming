eddc761c78b737e7670f556c8a488b76b1d616bd
Bouric Enos <bouricenos0@gmail.com>
2025-03-21 17:12:43 +0300

    Deleted sln file

--- StaticMethodApp/obj/Debug/net9.0/StaticMethodApp.AssemblyInfo.cs
+++ StaticMethodApp/obj/Debug/net9.0/StaticMethodApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaticMethodApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ef1b66cedafbc247aed02be738e69a510e19de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7cda2781540f74aafcb8c4b3c5c14ceb8566cc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaticMethodApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaticMethodApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
