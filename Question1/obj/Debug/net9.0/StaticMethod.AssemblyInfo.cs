26dbbdbc24f2a7424073062b87c2864a40a963f4
Bouric Enos <bouricenos0@gmail.com>
2025-05-13 10:38:41 +0300

    test program

--- Question1/obj/Debug/net9.0/StaticMethod.AssemblyInfo.cs
+++ Question1/obj/Debug/net9.0/StaticMethod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaticMethod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a46ed4a329d83b1095fababba92ae517d76934ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b12ed4857b90d55f6ca23c67b3e01e1824b4c3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaticMethod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaticMethod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
